90ca4117067e49c0c8c173de128685cc27390cb1
Michael McQuade <michael@giraffesyo.io>
2023-06-28 15:07:06 -0500

    fix: stop resource example

--- stopClusters.py
+++ stopClusters.py
@@ -11,15 +11,25 @@
                             .ssh directory.  Change permissions to mode 600.
 """
 
-import json,requests,sys,time,os
+import json
+import requests
+import sys
+import time
+import os
 from client import Client
 
 # inputs
-pw_url = "https://noaa.parallel.works"
+PW_PLATFORM_HOST = None
+if 'PW_PLATFORM_HOST' in os.environ:
+    PW_PLATFORM_HOST = os.environ['PW_PLATFORM_HOST']
+else:
+    print("No PW_PLATFORM_HOST environment variable found. Please set it to the Parallel Works platform host name. e.g. cloud.parallel.works")
+    sys.exit(1)
 
+pw_url = "https://" + PW_PLATFORM_HOST
 # specify the clusters to start and wait for activation
 #clusters = ["gcluster_noaa"]
-clusters = sys.argv[1].split(',')
+clusters_to_stop = sys.argv[1].split(',')
 
 # used to run test ssh commands after the clusters start
 # ensure your public key is added to the cluster configuration on Parallel Works
@@ -29,43 +39,49 @@ homedir = os.environ['HOME']
 keyfile = homedir + '/.ssh/pw_api.key'
 
 # get my personal API key
-try:
-  f = open(keyfile, "r")
-  api_key = f.readline().strip()
-  f.close()
-except IOError:
-  # Error out if there's no key
-  # could be improved by looking for an environment variable
-  # or asking the user to enter the key
-  print("Error: API file", keyfile, "does not appear to exist.")
-  sys.exit(1)
+# with the environment variable PW_API_KEY taking precedence
+# over the file $HOME/.ssh/pw_api.key
+api_key = None
+if 'PW_API_KEY' in os.environ:
+    api_key = os.environ['PW_API_KEY']
+else:
+    try:
+        f = open(keyfile, "r")
+        api_key = f.readline().strip()
+        f.close()
+    except:
+        pass
+
+if api_key is None or api_key == "":
+    print("No API key found. Please set the environment variable PW_API_KEY or create the file $HOME/.ssh/pw_api.key.")
+    sys.exit(1)
 
 # create a new Parallel Works client
-c = Client(pw_url,api_key)
+c = Client(pw_url, api_key)
 
 # get the account username
 account = c.get_account()
 
 user = account['info']['username']
-print("\nRunning as user",user+'...')
+print("\nRunning as user", user+'...')
+my_clusters = c.get_resources()
 
-for cluster_name in clusters:
+for cluster_name in clusters_to_stop:
 
-    print("\nChecking cluster status",cluster_name+"...")
+    print("\nChecking cluster status", cluster_name+"...")
 
     # check if resource exists and is on
-    cluster=c.get_resource(cluster_name)
+    cluster = next(
+        (item for item in my_clusters if item["name"] == cluster_name), None)
     if cluster:
         if cluster['status'] == "on":
             # if resource not on, start it
-            print("Stopping cluster",cluster_name+"...")
-            time.sleep(0.2)
-            print(c.stop_resource(cluster_name))
+            print("Stopping cluster", cluster['name']+"...")
+            print(c.stop_resource(cluster['id']))
         else:
-            print(cluster_name,"already stopped...")
+            print(cluster_name, "already stopped...")
     else:
         print("No cluster found.")
         sys.exit(1)
 
-print("\nStopped",len(clusters),"clusters...\n")
-
+print("\nStopped", len(clusters_to_stop), "clusters...\n")
